2d40b4ac333ee9cbb9959a0a8f2e3d12539eb4ba
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-10 16:43:43 -0500

    Finished first class

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ adventurer.roll = function roll (mod = 0) {
 console.log(adventurer);
 
 adventurer.roll()
+
+
+class Character{
+    constructor (name, roll){
+        this.name = name;
+        this.roll = roll;
+        this.health = 100;
+        this.inventory = [];
+    }
+}
+
+let robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory =["Small hat", "sunglasses"]
+
+robin.companion.roll;
