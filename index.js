ebd7692027612135c9ff117298496e4d5a6a44ea
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-10 16:36:34 -0500

    Added rolling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const adventurer ={
+let adventurer ={
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"]
@@ -7,3 +7,20 @@ const adventurer ={
 for (item of adventurer.inventory){
     console.log(item);
 }
+adventurer.companion = {};
+adventurer.companion.name = "Leo";
+adventurer.companion.type = "Cat";
+
+
+adventurer.companion.companion = {};
+adventurer.companion.companion.name = "Frank";
+adventurer.companion.companion.type = "Flea";
+
+
+adventurer.roll = function roll (mod = 0) {
+    const result = Math.floor(Math.random()*20)+1+mod;
+    console.log(`${this.name} rolled a ${result}.`)
+}
+console.log(adventurer);
+
+adventurer.roll()
